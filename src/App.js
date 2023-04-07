0283e0e5f187261e6c4dae211285fdb0bfb8628a
Siegfried <siegfried.bozza@renuo.ch>
2023-04-07 18:30:48 +0200

    setup react sass bs fa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  return (
+    <div className="">
+        <FontAwesomeIcon icon={faStar} />
+    </div>
+  );
+}
+
+export default App;
